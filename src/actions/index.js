902e9bf35fe3180c082bf55404df40e8a46808d3
Nate Magera <nmagera07@gmail.com>
2019-06-19 15:17:57 -0400

    init

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,19 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+import axios from 'axios'
+
+export const FETCH_DATA_START = 'FETCH_DATA_START'
+export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS'
+export const FETCH_DATA_ERROR = 'FETCH_DATA_ERROR'
+
+export const getData = dispatch => {
+    dispatch({ type: FETCH_DATA_START })
+    axios
+        .get(`https://swapi.co/api/people`)
+        .then(res => {
+            dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data})
+        })
+        .catch(err => dispatch({ type: FETCH_DATA_ERROR}))
+}
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
-import /* we need our action types here*/ "../actions";
+import {FETCH_DATA_START, FETCH_DATA_SUCCESS, FETCH_DATA_ERROR} from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  isLoading: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,26 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    
+      case FETCH_DATA_START:
+        return {
+          ...state,
+          isLoading: true,
+          error: ''
+        }
+      case FETCH_DATA_SUCCESS:
+        return {
+          ...state,
+          isLoading: false,
+          characters: action.payload
+        }
+      case FETCH_DATA_ERROR:
+        return {
+          ...state,
+          isLoading: false,
+          error: 'Sorry, something went wrong!'
+        }
+    
     default:
       return state;
   }
